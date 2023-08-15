73968268f369db35d59142ebf017a8df44f9a687
Julian Anaya <juliananaya531@gmail.com>
2023-08-15 16:43:44 -0700

    self practice q5

--- if.rb
+++ if.rb
@@ -15,21 +15,34 @@
 #   p 0
 # end
 
-# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
-num = 11
-num1 = 9
-if num < 10 && num1 < 10
-  p 1
-else
-  p 0
-end
+# num = 11
+# num1 = 9
+# if num < 10 && num1 < 10
+#   p 1
+# else
+#   p 0
+# end
 
-# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+# # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-number = 32042
-if number > 9000
-  p 1
+# number = 32042
+# if number > 9000
+#   p 1
+# else
+#   p -1
+# end
+
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+number = 44
+if number < 10
+  p 9
+elsif number < 20 
+  p 19
+elsif number < 30
+  p 29
 else
   p -1
 end
