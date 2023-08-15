173461b2558792efc86404b1e5ea79e2ae220019
Julian Anaya <juliananaya531@gmail.com>
2023-08-14 22:31:11 -0700

    self practice q2

--- if.rb
+++ if.rb
@@ -4,3 +4,13 @@ if numbers == 10
 else
   p -1
 end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+numbers = 10
+if numbers < 10
+  p -1
+elsif numbers > 10
+  p 1
+elsif numbers == 10
+  p 0
+end
