7df4610e0574dd1e62ccd7e83c7619bc5d3c1cf4
Julian Anaya <juliananaya531@gmail.com>
2023-08-16 16:56:35 -0700

    self practice strings q1

---
+++ string.rb
@@ -0,0 +1,6 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+first_name = "jane"
+last_name = "dane"
+
+p first_name.upcase + " " + last_name
