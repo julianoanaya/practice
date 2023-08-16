d568709283ec3a82ea62ce626853a7676afe4d03
Julian Anaya <juliananaya531@gmail.com>
2023-08-16 16:59:19 -0700

    self practice strings q2

--- string.rb
+++ string.rb
@@ -4,3 +4,10 @@ first_name = "jane"
 last_name = "dane"
 
 p first_name.upcase + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first_name = "john"
+last_name = "doe"
+
+p "#{first_name}" + " " + "#{ last_name }"
