2a4e114122f1ad9ba14663107e268f87b13f7e69
Julian Anaya <juliananaya531@gmail.com>
2023-08-15 20:11:26 -0700

    self practice q8

--- if.rb
+++ if.rb
@@ -65,3 +65,14 @@ if number < 0
 else
   p 1979
 end
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+number = 99
+if number == 100
+  p 100
+elsif number == 99
+  p 99
+else
+  p 0
+end
