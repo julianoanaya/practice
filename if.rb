0cda2c8599e5209a24f9dbd5e8872f2a5c3d723e
Julian Anaya <juliananaya531@gmail.com>
2023-08-15 20:05:18 -0700

    self practice q7

--- if.rb
+++ if.rb
@@ -56,3 +56,12 @@ if number > 10 || number1 > 10
 else
   p -100
 end
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+number = -34
+if number < 0
+  p 1776
+else
+  p 1979
+end
