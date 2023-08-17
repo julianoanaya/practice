6516694ac2b97c5f6749ab7245f5e876ac01d2cd
Julian Anaya <juliananaya531@gmail.com>
2023-08-16 20:39:56 -0700

    self practice strings q8

--- string.rb
+++ string.rb
@@ -49,3 +49,10 @@ author = "jacob doe"
 book = "the hunger games"
 
 p author + " wrote the best book ever written... " + book
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+author = "jane doe"
+book = "new world"
+
+p "#{author} wrote the best book... #{book}"
