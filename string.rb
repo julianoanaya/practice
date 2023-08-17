f290fc03f5317916917d3a826757ce1543e1e95e
Julian Anaya <juliananaya531@gmail.com>
2023-08-16 17:07:05 -0700

    self practice strings q4

--- string.rb
+++ string.rb
@@ -14,8 +14,16 @@ p "#{first_name}" + " " + "#{ last_name }"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-puts "Please enter a word."
-word = gets.chomp
-if word == "marco"
-  puts "polo"
-end
+# puts "Please enter a word."
+# word = gets.chomp
+# if word == "marco"
+#   puts "polo"
+# end
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+color1 = "blue"
+color2 = "red"
+color3 = "green"
+
+p "my favorite colors are" + " " + color1 + " and " + color2 + " and " + color3
