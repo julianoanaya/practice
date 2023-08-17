563468469299cd099f23086c91b27cf55add300c
Julian Anaya <juliananaya531@gmail.com>
2023-08-16 20:43:09 -0700

    self practice strings q9

--- string.rb
+++ string.rb
@@ -56,3 +56,14 @@ author = "jane doe"
 book = "new world"
 
 p "#{author} wrote the best book... #{book}"
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+puts "Please type in your password"
+password = gets.chomp
+
+if password == "Joshua"
+  puts "Shall we play a game"
+else
+  puts "Access denied"
+end
