f0ab42f4efa1bbcda360001cafc7485987330fd5
Julian Anaya <juliananaya531@gmail.com>
2023-08-30 10:33:07 -0700

    self practice methods q2

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -5,3 +5,8 @@ p word.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
+p "please type in a number"
+number = gets.chomp
+if number.to_i > 100
+  p "thats a big number"
+end
