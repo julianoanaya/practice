4220f1dd11fdb1242635c538b8cdea1f0539e8fa
Julian Anaya <juliananaya531@gmail.com>
2023-08-30 10:36:40 -0700

    self practice methods q3

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -10,3 +10,14 @@ number = gets.chomp
 if number.to_i > 100
   p "thats a big number"
 end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+p "please type in 2 numbers"
+number1 = gets.chomp
+number2 = gets.chomp
+
+p number1.to_i + number2.to_i 
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
