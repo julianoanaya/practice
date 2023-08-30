9d9adee425b84d0bfe5c0b3f18cc6ea9b86ccff8
Julian Anaya <juliananaya531@gmail.com>
2023-08-30 10:59:14 -0700

    self practice methods q10

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -54,7 +54,15 @@
 
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
-p "please type in 2 numbers"
-number1 = gets.chomp
-number2 = gets.chomp
-p number1.to_i * number2.to_i
+# p "please type in 2 numbers"
+# number1 = gets.chomp
+# number2 = gets.chomp
+# p number1.to_i * number2.to_i
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+p "please type in a word"
+word = gets.chomp
+if word.length > 5
+  p "thats a long word"
+end
