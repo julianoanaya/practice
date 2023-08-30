e39abe2f27dd99781af78dbb9d28d6ea10f36760
Julian Anaya <juliananaya531@gmail.com>
2023-08-30 10:29:10 -0700

    self practice methods q1

---
+++ methods_practice_problems.rb
@@ -0,0 +1,7 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+p "please type in a word"
+word = gets.chomp
+p word.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
