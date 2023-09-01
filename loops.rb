7ce68c23f90284f17d03cfaa71c35bcea9f9f424
Julian Anaya <juliananaya531@gmail.com>
2023-08-31 20:09:31 -0700

    self practice loops q4

--- loops.rb
+++ loops.rb
@@ -25,7 +25,7 @@
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 number = 0
-while number < 100
+while number <= 100
   p number
   number = number + 5
 end
