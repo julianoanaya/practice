87edb18cd0f23d63f2e979b7f8664b0c867168bb
Julian Anaya <juliananaya531@gmail.com>
2023-09-02 23:09:47 -0700

    self practice array q3

--- array.rb
+++ array.rb
@@ -7,7 +7,16 @@
 # p array
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-array = ["a","b","c","d"]
-p array
-array[1] = 1
-p array
+# array = ["a","b","c","d"]
+# p array
+# array[1] = 1
+# p array
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+array = [1,2,3,4,5]
+index = 0
+while index <= 4
+  p array[index]
+  index = index + 1
+end
