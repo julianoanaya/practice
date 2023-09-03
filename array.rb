1fec554d527b5fa132557d3a5e939fbcb4cb32e2
Julian Anaya <juliananaya531@gmail.com>
2023-09-02 23:06:23 -0700

    self practice array q2

--- array.rb
+++ array.rb
@@ -1,7 +1,13 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-array = ["heelo", "hi", "Sam",]
+# array = ["heelo", "hi", "Sam",]
+# p array
+# array << "ruby"
+# array << "star"
+# p array
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+array = ["a","b","c","d"]
 p array
-array << "ruby"
-array << "star"
+array[1] = 1
 p array
