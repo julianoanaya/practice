5f987f6a137eb16b03c98db86d17def2d52f47c8
Julian Anaya <juliananaya531@gmail.com>
2023-09-03 22:06:09 -0700

    self practice hash q2

--- hash.rb
+++ hash.rb
@@ -3,3 +3,13 @@ person = {"first_name" => "Sam", "last_name" => "Smoth", "email" => "ss@test"}
 p person["first_name"]
 p person["last_name"]
 p person["email"]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+people = [
+  { "first_name" => "Grace", "last_name" => "Hopper" },
+  { "first_name" => "Mace", "last_name" => "Windu" },
+  { "first_name" => "Robert", "last_name" => "Chase" }
+]
+p people[0]["first_name"]
+p people[0]["last_name"]
