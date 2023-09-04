6051321a74310c96d1ad694c65a1571563e589b6
Julian Anaya <juliananaya531@gmail.com>
2023-09-03 22:04:18 -0700

    self practice hash q1

---
+++ hash.rb
@@ -0,0 +1,5 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+person = {"first_name" => "Sam", "last_name" => "Smoth", "email" => "ss@test"}
+p person["first_name"]
+p person["last_name"]
+p person["email"]
