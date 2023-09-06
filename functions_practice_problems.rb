81d360b6fbbfe3f247902517c2274a94efc04894
Julian Anaya <juliananaya531@gmail.com>
2023-09-05 21:50:57 -0700

    self practice function practice q2

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -1,6 +1,12 @@
-# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+# # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-def get_double(number)
-  return number * 2
+# def get_double(number)
+#   return number * 2
+# end
+# p get_double(20)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+def all_caps(string)
+  return string.upcase
 end
-p get_double(20)
+print all_caps("hello")
