382f414dc2588082be31fb71106a14ca0a0cd06c
Julian Anaya <juliananaya531@gmail.com>
2023-09-05 21:48:36 -0700

    self practice function practice q1

---
+++ functions_practice_problems.rb
@@ -0,0 +1,6 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+def get_double(number)
+  return number * 2
+end
+p get_double(20)
