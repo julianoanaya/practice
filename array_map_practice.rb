cde2b097f79c878e0e0957f6a7dc54c51e8e441f
Julian Anaya <juliananaya531@gmail.com>
2023-09-06 22:25:15 -0700

    self practice array_map_practice q2

--- array_map_practice.rb
+++ array_map_practice.rb
@@ -1,9 +1,17 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
-new_numbers = []
-numbers.each do |number|
-  new_numbers << number * 3
+# numbers = [1, 2, 3]
+# new_numbers = []
+# numbers.each do |number|
+#   new_numbers << number * 3
+# end
+# p new_numbers
+
+#  2. Start with an array of strings and create a new array with each string upcased.
+#     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+strings = ["hello", "goodbye"]
+new_strings = strings.map do |string|
+  string.upcase
 end
-p new_numbers
+p new_strings
