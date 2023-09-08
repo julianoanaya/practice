5235c308599d4238cb888eb7e0bbf131d0a976dd
Julian Anaya <juliananaya531@gmail.com>
2023-09-07 23:58:17 -0700

    self arrays select practice q3

--- arrays_select_practice.rb
+++ arrays_select_practice.rb
@@ -8,8 +8,16 @@
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
-strings = ["winner", "winner", "chicken", "dinner"]
-w_strings = strings.select do |string|
-  string[0] == "w"
+# strings = ["winner", "winner", "chicken", "dinner"]
+# w_strings = strings.select do |string|
+#   string[0] == "w"
+# end
+# p w_strings
+
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+expensive_items = items.select do |item|
+  item[:price] > 5
 end
-p w_strings
+p expensive_items
