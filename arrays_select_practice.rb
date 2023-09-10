22ee42714cf64089617e06ab96c238d1e623f01a
Julian Anaya <juliananaya531@gmail.com>
2023-09-09 23:02:03 -0700

    self arrays select practice q9

--- arrays_select_practice.rb
+++ arrays_select_practice.rb
@@ -61,3 +61,11 @@ non_b_words = words.select do |word|
   word[0] != "b"
 end
 p non_b_words
+
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+inexpensive_items = items.select do |item|
+  item[:price] < 10
+end
+p inexpensive_items
