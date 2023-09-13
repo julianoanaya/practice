1ebaa965c7a3ca9e9f4d8187b18e0d6215c05017
Julian Anaya <juliananaya531@gmail.com>
2023-09-12 19:53:02 -0700

    arrays_reduce practice q5

--- arrays_reduce_practice.rb
+++ arrays_reduce_practice.rb
@@ -29,11 +29,20 @@
 
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
-numbers = [5, 10, 8, 3, 9]
-min_num = numbers[0]
-numbers.each do |number|
-  if number < min_num
-    min_num = number
-  end
+# numbers = [5, 10, 8, 3, 9]
+# min_num = numbers[0]
+# numbers.each do |number|
+#   if number < min_num
+#     min_num = number
+#   end
+# end
+# p min_num
+
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+words = ["volleyball", "basketball", "badminton"]
+number_of_letters = 0
+words.each do |word|
+  number_of_letters = number_of_letters + word.length
 end
-p min_num
+p number_of_letters
