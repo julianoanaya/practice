417e27d2eec3fa507d5c11ff2195b264c0fd3b59
Julian Anaya <juliananaya531@gmail.com>
2023-09-14 20:15:58 -0700

    converting_data_practice q3

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -13,11 +13,26 @@
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
-items = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
-items_hash = {}
+# items = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+# items_hash = {}
+# index = 0
+# while index < items.length
+#   items_hash[items[index][:id]] = items[index]
+#   index += 1
+# end
+# p items_hash
+
+#  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+#     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+word = "bookkeeper"
+letter_frequencies = {}
 index = 0
-while index < items.length
-  items_hash[items[index][:id]] = items[index]
+while index < word.length
+  letter = word[index]
+  if letter_frequencies[letter] == nil
+    letter_frequencies[letter] = 0
+  end
+  letter_frequencies[letter] += 1
   index += 1
 end
-p items_hash
+p letter_frequencies
