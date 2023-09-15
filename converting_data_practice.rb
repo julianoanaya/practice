0479a66d703e2fb3ddb923d2afedd0ff7ace3352
Julian Anaya <juliananaya531@gmail.com>
2023-09-14 20:19:21 -0700

    converting_data_practice q4

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -24,15 +24,24 @@
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
-word = "bookkeeper"
-letter_frequencies = {}
-index = 0
-while index < word.length
-  letter = word[index]
-  if letter_frequencies[letter] == nil
-    letter_frequencies[letter] = 0
-  end
-  letter_frequencies[letter] += 1
-  index += 1
+# word = "bookkeeper"
+# letter_frequencies = {}
+# index = 0
+# while index < word.length
+#   letter = word[index]
+#   if letter_frequencies[letter] == nil
+#     letter_frequencies[letter] = 0
+#   end
+#   letter_frequencies[letter] += 1
+#   index += 1
+# end
+# p letter_frequencies
+
+#  4. Convert a hash into an array of arrays.
+#     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+things = { "chair" => 100, "book" => 14 }
+name_price_pairs = []
+things.each do |name, price|
+  name_price_pairs << [name, price]
 end
-p letter_frequencies
+p name_price_pairs
