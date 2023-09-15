a9cc197e024a70e5653ccf4a0331c46f0bcb5229
Julian Anaya <juliananaya531@gmail.com>
2023-09-14 20:12:01 -0700

    converting_data_practice q2

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -1,12 +1,23 @@
 #  1. Convert an array of arrays into a hash.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
-pairs = [[1, 3], [8, 9], [2, 16]]
-pairs_hash = {}
+# pairs = [[1, 3], [8, 9], [2, 16]]
+# pairs_hash = {}
+# index = 0
+# while index < pairs.length
+#   key = pairs[index][0]
+#   value = pairs[index][1]
+#   pairs_hash[key] = value
+#   index = index + 1
+# end
+# p pairs_hash
+
+#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+#     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+items = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+items_hash = {}
 index = 0
-while index < pairs.length
-  key = pairs[index][0]
-  value = pairs[index][1]
-  pairs_hash[key] = value
-  index = index + 1
+while index < items.length
+  items_hash[items[index][:id]] = items[index]
+  index += 1
 end
-p pairs_hash
+p items_hash
