b3a435de7dd6049df3eae36783d9a4d7098b91ef
Julian Anaya <juliananaya531@gmail.com>
2023-09-15 22:26:26 -0700

    converting_data_practice q8

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -73,10 +73,27 @@
 
 #  7. Convert a hash into a flat array containing all the hash’s keys and values.
 #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
-hash = { "a" => 1, "b" => 2, "c" => 3, "d" => 4 }
-flattened_array = []
-hash.each do |key, value|
-  flattened_array << key
-  flattened_array << value
+# hash = { "a" => 1, "b" => 2, "c" => 3, "d" => 4 }
+# flattened_array = []
+# hash.each do |key, value|
+#   flattened_array << key
+#   flattened_array << value
+# end
+# p flattened_array
+
+#  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+#     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+price_hash = { "chair" => 75, "book" => 15 }
+items = [{ name: "chair", color: "red", weight: 10 }, { name: "book", color: "black", weight: 1 }]
+combined_hash = {}
+index = 0
+while index < items.length
+  item = items[index]
+  name = item[:name]
+  color = item[:color]
+  weight = item[:weight]
+  price = price_hash[name]
+  combined_hash[name] = { price: price, color: color, weight: weight }
+  index += 1
 end
-p flattened_array
+p combined_hash
