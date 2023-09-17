cb3793ac167c3e5e3903ebda2b3d1ec98200956a
Julian Anaya <juliananaya531@gmail.com>
2023-09-17 00:11:57 -0700

    python practice if statement q2

--- python_practice.py
+++ python_practice.py
@@ -5,3 +5,10 @@ if number == 10:
   print(0)
 else:
   print(1)
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+number = 20
+if number < 10:
+  print(1)
+else:
+  print(0)
