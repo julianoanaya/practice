df3fe1113f5edabc88a996537367c26466da4cd9
Julian Anaya <juliananaya531@gmail.com>
2023-09-17 14:39:06 -0700

    python practice if statement q5

--- python_practice.py
+++ python_practice.py
@@ -12,9 +12,9 @@
 #   print(1)
 # else:
 #   print(0)
-  
-  # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-  
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
 # number = 2
 # number_2 = 2
 # if number < 10 & number_2 < 10:
@@ -23,8 +23,21 @@
 #     print("0")
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-number = 34
-if number > 9000:
-  print(1)
+# number = 34
+# if number > 9000:
+#   print(1)
+# else:
+#   print(-1)
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
+number = 33
+if number < 10:
+    print(9)
+elif number < 20:
+    print(19)
+elif number < 30:
+    print(29)
 else:
-  print(-1)
+    print(-1)
