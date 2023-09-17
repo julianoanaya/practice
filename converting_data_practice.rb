8a0cf31a750650f8bc239b4035e3640de2bd9da7
Julian Anaya <juliananaya531@gmail.com>
2023-09-17 00:08:29 -0700

    python practice if statement q1

--- converting_data_practice.rb
+++ converting_data_practice.rb
@@ -117,9 +117,9 @@
 
 # 10. Given a hash, create a new hash that has the keys and values switched.
 #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
-original_hash = { "a" => 1, "b" => 2, "c" => 3 }
-flipped_hash = {}
-original_hash.each do |key, value|
-  flipped_hash[value] = key
-end
-p flipped_hash
+# original_hash = { "a" => 1, "b" => 2, "c" => 3 }
+# flipped_hash = {}
+# original_hash.each do |key, value|
+#   flipped_hash[value] = key
+# end
+# p flipped_hash
---
+++ python_practice.py
@@ -0,0 +1,7 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+number = 1
+
+if number == 10:
+  print(0)
+else:
+  print(1)
