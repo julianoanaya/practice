b02adec470f11e1545eca96d45831704327047fa
Julian Anaya <juliananaya531@gmail.com>
2023-09-22 20:31:25 -0700

    python string practice  q1

---
+++ strings_practice_python.py
@@ -0,0 +1,6 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+First_name = "Sam "
+Last_name = "Smith"
+
+print(First_name + Last_name)
