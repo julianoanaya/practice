c34e2169824a84cacdd4c8d00fb37bc7d9b92bf2
Julian Anaya <juliananaya531@gmail.com>
2023-09-22 20:42:18 -0700

    python string practice  q3

--- strings_practice_python.py
+++ strings_practice_python.py
@@ -7,7 +7,18 @@
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = "John"
-last_name = "Doe"
+# first_name = "John"
+# last_name = "Doe"
 
-print(f"{first_name} {last_name}")
+# print(f"{first_name} {last_name}")
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".m
+
+# Get the user's input
+word = input("Enter a word: ")
+
+# Check if the word is "marco"
+if word == "marco":
+    print("polo!")
+else:
+    print("Not marco.")
