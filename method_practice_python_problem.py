95cd816239a1ffde15af38052924c45224be201b
Julian Anaya <juliananaya531@gmail.com>
2023-09-23 23:19:53 -0700

    python method practice  q3

--- method_practice_python_problem.py
+++ method_practice_python_problem.py
@@ -4,8 +4,17 @@
 # print(word.upper())
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
-number = input("please type in a number: ")
-if int(number) > 100:
-    print("Thats a big a number")
-else:
-    print("Thats a small number")
+# number = input("please type in a number: ")
+# if int(number) > 100:
+#     print("Thats a big a number")
+# else:
+#     print("Thats a small number")
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+
+sum = int(number1) + int(number2)
+
+print("The sum of the two numbers is:", sum)
