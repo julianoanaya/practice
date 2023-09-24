e1d3bbdebb23a978f4b14191f9423b0be5f01cc8
Julian Anaya <juliananaya531@gmail.com>
2023-09-23 23:16:46 -0700

    python method practice  q2

--- method_practice_python_problem.py
+++ method_practice_python_problem.py
@@ -1,4 +1,11 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-word = input("Enter a word: ")
-print(word.upper())
+# word = input("Enter a word: ")
+# print(word.upper())
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+number = input("please type in a number: ")
+if int(number) > 100:
+    print("Thats a big a number")
+else:
+    print("Thats a small number")
