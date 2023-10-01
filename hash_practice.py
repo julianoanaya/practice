2e921882742aa7b8e5759f9cd9273107214a52f3
Julian Anaya <juliananaya531@gmail.com>
2023-09-30 21:33:53 -0700

    python hash practice q2

--- hash_practice.py
+++ hash_practice.py
@@ -1,6 +1,15 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
-person = {"first_name": "John", "last_name": "Doe", "email": "johndoe@example.com"}
+# person = {"first_name": "John", "last_name": "Doe", "email": "johndoe@example.com"}
 
-print(person["first_name"])
-print(person["last_name"])
-print(person["email"])
+# print(person["first_name"])
+# print(person["last_name"])
+# print(person["email"])
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+people = [
+    {"first_name": "John", "last_name": "Doe"},
+    {"first_name": "Jane", "last_name": "Smith"},
+    {"first_name": "Peter", "last_name": "Jones"},
+]
+
+print(people[0])
