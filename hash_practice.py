fa82cf608d04d592e26b5946581db042f72bdc45
Julian Anaya <juliananaya531@gmail.com>
2023-09-30 21:42:34 -0700

    python hash practice q4

--- hash_practice.py
+++ hash_practice.py
@@ -6,16 +6,29 @@
 # print(person["email"])
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
-people = [
-    {"first_name": "John", "last_name": "Doe"},
-    {"first_name": "Jane", "last_name": "Smith"},
-    {"first_name": "Peter", "last_name": "Jones"},
-]
+# people = [
+#     {"first_name": "John", "last_name": "Doe"},
+#     {"first_name": "Jane", "last_name": "Smith"},
+#     {"first_name": "Peter", "last_name": "Jones"},
+# ]
 
-print(people[0])
+# print(people[0])
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
-menu_items = {"burger": 10, "french fries": 3, "drink": 2}
-menu_items["combo"] = 13
-print(menu_items)
+# menu_items = {"burger": 10, "french fries": 3, "drink": 2}
+# menu_items["combo"] = 13
+# print(menu_items)
+
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+book = {
+    "title": "The Greatest Book Ever Written",
+    "author": "John Doe",
+    "number_of_pages": 413,
+    "language": "Greek",
+}
+print(book["title"])
+print(book["author"])
+print(book["number_of_pages"])
+print(book["language"])
