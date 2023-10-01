dcb002f303c2dc766b9f7229c790d28bf97a20d0
Julian Anaya <juliananaya531@gmail.com>
2023-09-30 21:26:59 -0700

    python hash practice q1

---
+++ hash_practice.py
@@ -0,0 +1,6 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+person = {"first_name": "John", "last_name": "Doe", "email": "johndoe@example.com"}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["email"])
--- loop.py
+++ loop.py
@@ -59,3 +59,5 @@ while True:
     if len(word) > 5:
         break
     print("The word you entered is too short.")
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
