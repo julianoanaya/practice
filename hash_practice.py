bdac65118b3e765b08fa03f44b17e0815c7ce0fe
Julian Anaya <juliananaya531@gmail.com>
2023-10-01 23:10:37 -0700

    python hash practice q7

--- hash_practice.py
+++ hash_practice.py
@@ -34,16 +34,23 @@
 # print(book["language"])
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
-books = [
-    {"title": "The Greatest Book Ever", "author": "James. Smith"},
-    {"title": "The Worst Book Ever", "author": "Joe Doe"},
-    {"title": "The Most Average Book Ever", "author": "John Mart"},
-]
+# books = [
+#     {"title": "The Greatest Book Ever", "author": "James. Smith"},
+#     {"title": "The Worst Book Ever", "author": "Joe Doe"},
+#     {"title": "The Most Average Book Ever", "author": "John Mart"},
+# ]
 
-print(books[2]["author"])
+# print(books[2]["author"])
 
-# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+# # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
-states = {"California": "Sacramento", "Florida": "Orlando", "Texas": "Austin"}
-states["Nevada"] = "Carson City"
-print(states)
+# states = {"California": "Sacramento", "Florida": "Orlando", "Texas": "Austin"}
+# states["Nevada"] = "Carson City"
+# print(states)
+
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+laptop = {"brand": "Apple", "model": "MacBook Pro", "year": 2023}
+print("brand")
+print("model")
+print("year")
