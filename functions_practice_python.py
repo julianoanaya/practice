caa3a1595daad9ad7ca105f1c1715732379a2f39
Julian Anaya <juliananaya531@gmail.com>
2023-10-02 20:42:15 -0700

    python function practice q2

--- functions_practice_python.py
+++ functions_practice_python.py
@@ -1,7 +1,15 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
-def times_two(number):
-    return number * 2
+# def times_two(number):
+#     return number * 2
 
 
-result = times_two(5)
-print(result)
+# result = times_two(5)
+# print(result)
+
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+def to_upper(string):
+    return string.upper()
+
+
+print(to_upper("hello"))
