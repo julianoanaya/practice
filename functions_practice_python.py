d82ca834b32084c9cff5109437549d3549124539
Julian Anaya <juliananaya531@gmail.com>
2023-10-02 20:36:50 -0700

    python function practice q1

---
+++ functions_practice_python.py
@@ -0,0 +1,7 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+def times_two(number):
+    return number * 2
+
+
+result = times_two(5)
+print(result)
