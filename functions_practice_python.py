91bedafe2bd07443f0d359db7ba8a5b40013a555
Julian Anaya <juliananaya531@gmail.com>
2023-10-02 20:47:40 -0700

    python function practice q3

--- functions_practice_python.py
+++ functions_practice_python.py
@@ -8,8 +8,18 @@
 
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
-def to_upper(string):
-    return string.upper()
+# def to_upper(string):
+#     return string.upper()
 
 
-print(to_upper("hello"))
+# print(to_upper("hello"))
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+
+def subtract_numbers(number1, number2):
+    return number1 - number2
+
+
+result = subtract_numbers(10, 5)
+print(result)
