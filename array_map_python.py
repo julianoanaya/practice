b385e768e68a66583a46990eb4f2436097236b31
Julian Anaya <juliananaya531@gmail.com>
2023-10-04 23:14:13 -0700

    python array map  practice q1

---
+++ array_map_python.py
@@ -0,0 +1,7 @@
+#  1. Start with an array of numbers and create a new array with each number times 3.
+#     For example, [1, 2, 3] becomes [3, 6, 9].
+
+
+array = [1, 2, 3]
+new_array = [number * 3 for number in array]
+print(new_array)
