f6a0780dda964c970d945dd798d4eaae8b09813a
Julian Anaya <juliananaya531@gmail.com>
2023-10-04 23:25:51 -0700

    python array map  practice q4

--- array_map_python.py
+++ array_map_python.py
@@ -16,8 +16,16 @@
 #  3. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
-array = [1, 2, 3]
+# array = [1, 2, 3]
 
-new_array = [number + 7 for number in array]
+# new_array = [number + 7 for number in array]
 
-print(new_array)
+# print(new_array)
+
+# 4. Start with an array of strings and create a new array with each string's length.
+#     For example, ["hello", "goodbye"] becomes [5, 7].
+strings = ["hello", "goodbye"]
+
+lengths = [len(string) for string in strings]
+
+print(lengths)
