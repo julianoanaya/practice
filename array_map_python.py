72ef8071a47a7e627f3cbd8f72aa501a7d55dafd
Julian Anaya <juliananaya531@gmail.com>
2023-10-06 22:08:11 -0700

    python array map  practice q5

--- array_map_python.py
+++ array_map_python.py
@@ -24,8 +24,18 @@
 
 # 4. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
-strings = ["hello", "goodbye"]
+# strings = ["hello", "goodbye"]
 
-lengths = [len(string) for string in strings]
+# lengths = [len(string) for string in strings]
 
-print(lengths)
+# print(lengths)
+
+
+#  5. Start with an array of numbers and create a new array with each number divided by 2.
+#     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+array = [1, 2, 3]
+
+# Divide each number in the array by 2
+new_array = [number / 2 for number in array]
+
+print(new_array)
