5dc4b5145f47567accfb0e8175313e924caccec8
Julian Anaya <juliananaya531@gmail.com>
2023-10-10 23:10:42 -0700

    python select arrays  practice q2

--- arrays_select_python_practice.py
+++ arrays_select_python_practice.py
@@ -5,3 +5,9 @@ numbers = [2, 32, 80, 18, 12, 3]
 new_array = [number for number in numbers if number < 20]
 
 print(new_array)
+
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+array = ["hello", "world", "goodbye"]
+new_array = [string for string in array if string.startswith("w")]
+print(new_array)
