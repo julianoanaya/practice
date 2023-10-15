e234057ca14985d4882d495b1b4c4074b600f8cb
Julian Anaya <juliananaya531@gmail.com>
2023-10-14 23:13:35 -0700

    python select arrays  practice q6

--- arrays_select_python_practice.py
+++ arrays_select_python_practice.py
@@ -1,40 +1,51 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
-numbers = [2, 32, 80, 18, 12, 3]
+# numbers = [2, 32, 80, 18, 12, 3]
 
-new_array = [number for number in numbers if number < 20]
+# new_array = [number for number in numbers if number < 20]
 
-print(new_array)
+# print(new_array)
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
-array = ["hello", "world", "goodbye"]
-new_array = [string for string in array if string.startswith("w")]
-print(new_array)
+# array = ["hello", "world", "goodbye"]
+# new_array = [string for string in array if string.startswith("w")]
+# print(new_array)
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100},
-hashes = [
-    {"name": "John Doe", "price": 10},
-    {"name": "Jane Doe", "price": 20},
-    {"name": "Peter Smith", "price": 0},
-]
+# hashes = [
+#     {"name": "John Doe", "price": 10},
+#     {"name": "Jane Doe", "price": 20},
+#     {"name": "Peter Smith", "price": 0},
+# ]
 
-new_hashes = [hash for hash in hashes if hash["price"] > 5]
+# new_hashes = [hash for hash in hashes if hash["price"] > 5]
 
-print(new_hashes)
+# print(new_hashes)
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-even_numbers = [number for number in numbers if number % 2 == 0]
+# even_numbers = [number for number in numbers if number % 2 == 0]
 
-print(even_numbers)
+# print(even_numbers)
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
-strings = ["Hello", "World", "My", "Name", "Is", "Bard"]
+# strings = ["Hello", "World", "My", "Name", "Is", "Bard"]
 
-short_strings = [string for string in strings if len(string) < 4]
+# short_strings = [string for string in strings if len(string) < 4]
 
-print(short_strings)
+# print(short_strings)
+
+
+array = [
+    {"name": "John", "age": 30},
+    {"name": "Jane ", "age": 25},
+    {"name": "Peter ", "age": 40},
+]
+
+new_array = filter(lambda hash: len(hash["name"]) < 6, array)
+
+print(new_array)
