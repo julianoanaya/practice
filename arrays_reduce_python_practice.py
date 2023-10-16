229ca3fbc31cb3c8ef81ff2123ed2c7bd0331299
Julian Anaya <juliananaya531@gmail.com>
2023-10-15 23:16:39 -0700

    python arrays reduce q2

--- arrays_reduce_python_practice.py
+++ arrays_reduce_python_practice.py
@@ -1,7 +1,16 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 
-array = [1, 2, 3, 4, 5]
-sum = 0
-for number in array:
-    sum += number
-print(sum)
+# array = [1, 2, 3, 4, 5]
+# sum = 0
+# for number in array:
+#     sum += number
+# print(sum)
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+array = ["string1", "string2", "string3"]
+
+combined_string = "".join(array)
+
+print(combined_string)
