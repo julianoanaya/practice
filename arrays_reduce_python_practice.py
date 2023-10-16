35e1da48339dfc699745234df7175092493583ea
Julian Anaya <juliananaya531@gmail.com>
2023-10-15 23:24:43 -0700

    python arrays reduce q4

--- arrays_reduce_python_practice.py
+++ arrays_reduce_python_practice.py
@@ -18,8 +18,16 @@
 # 3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
-array = [{"price": 10}, {"price": 20}, {"price": 30}]
+# array = [{"price": 10}, {"price": 20}, {"price": 30}]
 
-sum_of_prices = sum([item["price"] for item in array])
+# sum_of_prices = sum([item["price"] for item in array])
 
-print(sum_of_prices)
+# print(sum_of_prices)
+
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3, 9] becomes 3.
+
+array = [5, 10, 8, 3, 9]
+min_array = min(array)
+print(min_array)
