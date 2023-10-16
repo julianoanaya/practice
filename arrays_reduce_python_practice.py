6f197d493741a7f99763bc460d0ff00d3a91bcf2
Julian Anaya <juliananaya531@gmail.com>
2023-10-15 23:21:00 -0700

    python arrays reduce q3

--- arrays_reduce_python_practice.py
+++ arrays_reduce_python_practice.py
@@ -9,8 +9,17 @@
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
-array = ["string1", "string2", "string3"]
+# array = ["string1", "string2", "string3"]
 
-combined_string = "".join(array)
+# combined_string = "".join(array)
 
-print(combined_string)
+# print(combined_string)
+
+# 3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+array = [{"price": 10}, {"price": 20}, {"price": 30}]
+
+sum_of_prices = sum([item["price"] for item in array])
+
+print(sum_of_prices)
